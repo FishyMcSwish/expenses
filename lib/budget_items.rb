80cfdf989b90679afdd48849abb7578958f4aacf
Grant Maki <grant.maki@springhealth.com>
2022-10-18 10:21:53 -0400

    fixed bug with adding savings to all accounts, got rubocop working

--- lib/budget_items.rb
+++ lib/budget_items.rb
@@ -1,121 +1,159 @@
 # frozen_string_literal: true
-require "csv"
 
-  class Utils
-    def self.to_hash(record)
-      hash = {}
-      record.instance_variables.each do |var| 
-        hash[var.to_s.delete("@")] = record.instance_variable_get(var)
-      end
-      hash
+require 'csv'
+
+class Utils
+  def self.to_hash(record)
+    hash = {}
+    record.instance_variables.each do |var|
+      hash[var.to_s.delete('@')] = record.instance_variable_get(var)
     end
+    hash
   end
-  INFLATION_RATE = 0.03
-  INFINITE_DURATION = :infinite
-  EXPIRED_DURATION = :expired
+end
+INFLATION_RATE = 0.03
+INFINITE_DURATION = :infinite
+EXPIRED_DURATION = :expired
+##
+# base class for expenses and income
+class BudgetItem
+  attr_accessor :name, :amount, :rate_of_increase, :duration
+
+  def initialize(name, amount, rate_of_increase = INFLATION_RATE, duration = INFINITE_DURATION)
+    @name = name
+    @amount = amount
+    @rate_of_increase = rate_of_increase
+    @duration = duration
+  end
+
   ##
-  # base class for expenses and income
-  class BudgetItem
-    attr_accessor :name, :amount, :rate_of_increase, :duration
-
-    def initialize(name, amount, rate_of_increase=INFLATION_RATE, duration=INFINITE_DURATION)
-      @name = name
-      @amount = amount
-      @rate_of_increase = rate_of_increase 
-      @duration = duration
-    end
-
-    ##
-    # a doc
-    def annual_increase()
-      new_duration = (self.duration.is_a? Integer) ? self.duration - 1 : self.duration
-      new_amount = self.amount * self.rate_of_increase + self.amount
-      if(new_duration == EXPIRED_DURATION || new_duration == 0)
-        return BudgetItem.new(self.name, 0, 0, EXPIRED_DURATION)
-      else 
-        return BudgetItem.new(self.name, new_amount, self.rate_of_increase, new_duration)
-      end
-    end
-
-    def isExpired?()
-      self.duration == EXPIRED_DURATION
-    end
-
-    def self.recurring_expense(name, amount)
-      BudgetItem.new(name, -amount)
-    end
-    def self.one_time_expense(name, amount)
-      BudgetItem.new(name, -amount, 0, 1)
-    end
-
-    def self.recurring_income(name, amount)
-      BudgetItem.new(name, amount)
-    end
-    def self.one_time_income(name, amount)
-      BudgetItem.new(name, amount, 0, 1)
-    end
-
-    def ==(other)
-      if(other.nil? && self.nil?)
-        return true
-      elsif(self.nil?)
-        return false
-      else
-        return self.name == other.name && self.amount == other.amount && self.rate_of_increase == other.rate_of_increase && self.duration = other.duration
-      end
-    end
-    def to_h
-      Utils.to_hash(self)
-    end
-
-  end
-
-  class Year
-
-    attr_accessor :items
-    def initialize(items)
-      @items = items
-    end
-    def nextYear()
-      Year.new(self.items.map { |item| item.annual_increase()})
-    end
-    def extraCash()
-      self.items.reduce(0){|sum, item| sum + item.amount}
-    end
-    def merge(other)
-      other.nil? ? self : Year.new(self.items + other.items)
-    end
-    def to_h
-      {"items" => items.map{ |item| item.to_h}}
+  # a doc
+  def annual_increase
+    new_duration = (duration.is_a? Integer) ? duration - 1 : duration
+    new_amount = amount * rate_of_increase + amount
+    if [EXPIRED_DURATION, 0].include?(new_duration)
+      BudgetItem.new(name, 0, 0, EXPIRED_DURATION)
+    else
+      BudgetItem.new(name, new_amount, rate_of_increase, new_duration)
     end
   end
 
-  class Plan
-    attr_accessor :current_year
-    def initialize(years, current_year=0)
-      @years = years
-      @current_year = current_year
-    end
+  def isExpired?
+    duration == EXPIRED_DURATION
+  end
 
-    def years
-      Hash[@years.sort_by {|k,v| k}]
-    end
-    
-    def run_years(num_of_years)
-      current_year = self.current_year
-      new_years = @years.merge
-      while current_year < num_of_years
-        next_year = new_years[current_year].nextYear()
-        current_year += 1
-        existing_year = new_years[current_year]
-        new_years[current_year] = next_year.merge(existing_year)
-      end
-      Plan.new(new_years, current_year)
-    end
+  def self.recurring_expense(name, amount)
+    BudgetItem.new(name, -amount)
+  end
 
-    def toCSV
-      years.map {|k, year| [k, year.to_h]}.to_h
+  def self.one_time_expense(name, amount)
+    BudgetItem.new(name, -amount, 0, 1)
+  end
+
+  def self.recurring_income(name, amount)
+    BudgetItem.new(name, amount)
+  end
+
+  def self.one_time_income(name, amount)
+    BudgetItem.new(name, amount, 0, 1)
+  end
+
+  def ==(other)
+    if other.nil? && nil?
+      true
+    elsif nil?
+      false
+    else
+      name == other.name && amount == other.amount && rate_of_increase == other.rate_of_increase && self.duration = other.duration
     end
+  end
+
+  def to_h
+    Utils.to_hash(self)
+  end
 end
 
+class Account
+  attr_accessor :name, :amount, :rate_of_increase
 
+  def initialize(name, amount, rate_of_increase)
+    @name = name
+    @amount = amount
+    @rate_of_increase = rate_of_increase
+  end
+
+  def annual_increase
+    Account.new(@name, @amount + @amount * @rate_of_increase, @rate_of_increase)
+  end
+
+  def add(change)
+    Account.new(name, amount + change, rate_of_increase)
+  end
+
+  def ==(other)
+    if other.nil? && nil?
+      true
+    elsif nil?
+      false
+    else
+      name == other.name && amount == other.amount && rate_of_increase == other.rate_of_increase
+    end
+  end
+end
+
+class Year
+  attr_accessor :items, :accounts
+
+  def initialize(items = [], accounts = [])
+    @items = items
+    @accounts = accounts
+  end
+
+  def nextYear
+    increased_accounts = accounts.map do |acct|
+      acct.name == 'investments' ? acct.annual_increase.add(extraCash) : acct.annual_increase
+    end
+    Year.new(items.map(&:annual_increase), increased_accounts)
+  end
+
+  def extraCash
+    items.reduce(0) { |sum, item| sum + item.amount }
+  end
+
+  def merge(other)
+    other.nil? ? self : Year.new(items + other.items, accounts + other.accounts)
+  end
+
+  def to_h
+    { 'items' => items.map { |item| item.to_h } }
+  end
+end
+
+class Plan
+  attr_accessor :current_year
+
+  def initialize(years, current_year = 0)
+    @years = years
+    @current_year = current_year
+  end
+
+  def years
+    Hash[@years.sort_by { |k, _v| k }]
+  end
+
+  def run_years(num_of_years)
+    current_year = self.current_year
+    new_years = @years.merge
+    while current_year < num_of_years
+      nextYear = new_years[current_year].nextYear
+      current_year += 1
+      existing_year = new_years[current_year]
+      new_years[current_year] = nextYear.merge(existing_year)
+    end
+    Plan.new(new_years, current_year)
+  end
+
+  def toCSV
+    years.map { |k, year| [k, year.to_h] }.to_h
+  end
+end
--- spec/expenses_spec.rb
+++ spec/expenses_spec.rb
@@ -1,88 +1,108 @@
 # frozen_string_literal: true
+
 require 'budget_items'
+require 'data'
 
 describe 'Finances' do
-  
   describe 'Budget Items' do
     describe 'constructors' do
       it 'has default params' do
-        expect(BudgetItem.new("test", 100)).to eq(BudgetItem.new("test", 100, INFLATION_RATE, INFINITE_DURATION))
+        expect(BudgetItem.new('test', 100)).to eq(BudgetItem.new('test', 100, INFLATION_RATE, INFINITE_DURATION))
       end
-      it 'expenses are negative' do 
-        exp = BudgetItem.recurring_expense("kids", 100)
-        expect(exp).to eq(BudgetItem.new("kids", -100))
+      it 'expenses are negative' do
+        exp = BudgetItem.recurring_expense('kids', 100)
+        expect(exp).to eq(BudgetItem.new('kids', -100))
       end
 
       it 'deposits are positive' do
-        income = BudgetItem.recurring_income("work", 100)
-        expect(income).to eq(BudgetItem.new("work", 100))
+        income = BudgetItem.recurring_income('work', 100)
+        expect(income).to eq(BudgetItem.new('work', 100))
       end
-      
-      it "expired items are expired" do
-        item = BudgetItem.new("test", 0, 0, EXPIRED_DURATION)
+
+      it 'expired items are expired' do
+        item = BudgetItem.new('test', 0, 0, EXPIRED_DURATION)
         expect(item.isExpired?).to eq(true)
       end
-      it "items with duration left are not expired" do
-        item1 = BudgetItem.new("test", 0, 0, 1)
-        item2 = BudgetItem.new("test", 0, 0, INFINITE_DURATION)
+      it 'items with duration left are not expired' do
+        item1 = BudgetItem.new('test', 0, 0, 1)
+        item2 = BudgetItem.new('test', 0, 0, INFINITE_DURATION)
         expect(item1.isExpired?).to eq(false)
         expect(item2.isExpired?).to eq(false)
       end
-      it "one time expenses" do
-        exp = BudgetItem.one_time_expense("kids", 100)
-        expect(exp).to eq(BudgetItem.new("kids", -100, 0, 1))
+      it 'one time expenses' do
+        exp = BudgetItem.one_time_expense('kids', 100)
+        expect(exp).to eq(BudgetItem.new('kids', -100, 0, 1))
       end
-      it "one time income" do
-        exp = BudgetItem.one_time_income("kids", 100)
-        expect(exp).to eq(BudgetItem.new("kids", 100, 0, 1))
+      it 'one time income' do
+        exp = BudgetItem.one_time_income('kids', 100)
+        expect(exp).to eq(BudgetItem.new('kids', 100, 0, 1))
       end
     end
     describe 'annual increases' do
       it 'increase at the specified rate' do
-        item = BudgetItem.new("test", 100, 0.05)
+        item = BudgetItem.new('test', 100, 0.05)
         expect(item.annual_increase.amount).to eq(105)
       end
       it "doesn't change the duration on infinite duration items" do
-        year1 = BudgetItem.recurring_expense("test", 100)
+        year1 = BudgetItem.recurring_expense('test', 100)
         year2 = year1.annual_increase
         expect(year2.duration).to eq(INFINITE_DURATION)
       end
-      it "changes the duration for finite duration items" do
-        year1 = BudgetItem.new("test", 100, INFLATION_RATE, 100)
+      it 'changes the duration for finite duration items' do
+        year1 = BudgetItem.new('test', 100, INFLATION_RATE, 100)
         year2 = year1.annual_increase
         expect(year2.duration).to eq(99)
       end
-      it "returns an expired expense if the duration is over" do
-        year1 = BudgetItem.new("test", 100, INFLATION_RATE, 1)
+      it 'returns an expired expense if the duration is over' do
+        year1 = BudgetItem.new('test', 100, INFLATION_RATE, 1)
         year2 = year1.annual_increase
-        expect(year2).to eq(BudgetItem.new("test", 0, 0, EXPIRED_DURATION))
+        expect(year2).to eq(BudgetItem.new('test', 0, 0, EXPIRED_DURATION))
       end
     end
     it 'to_h' do
-      item = BudgetItem.new("test", 100, INFLATION_RATE, INFINITE_DURATION)
-      expect(item.to_h).to eq({"name" => "test", "amount" => 100, "rate_of_increase" => INFLATION_RATE, "duration" => :infinite})
+      item = BudgetItem.new('test', 100, INFLATION_RATE, INFINITE_DURATION)
+      expect(item.to_h).to eq({ 'name' => 'test', 'amount' => 100, 'rate_of_increase' => INFLATION_RATE,
+                                'duration' => :infinite })
     end
   end
-  
+
+  describe 'Accounts' do
+    it 'does an annual increase' do
+      acct = Account.new('investments', 100, -0.05)
+      expect(acct.annual_increase).to eq(Account.new('investments', 95, -0.05))
+    end
+  end
+
   describe 'Years' do
     describe 'nextYear' do
       it 'increases all the items' do
-        year1 = Year.new([BudgetItem.recurring_expense("kids", 100), BudgetItem.recurring_income("work", 100)])
-        year2 = year1.nextYear()
-        expect(year2.items.map {|item| item.amount}).to  eq([-103, 103])
+        year1 = Year.new([BudgetItem.recurring_expense('kids', 100), BudgetItem.recurring_income('work', 100)],
+                         [Account.new('acct', 100, 0.05)])
+        year2 = year1.nextYear
+        expect(year2.items.map(&:amount)).to eq([-103, 103])
+        expect(year2.accounts.map(&:amount)).to eq([105])
+      end
+      it 'adds extra cash to investment account' do
+        year1 = Year.new([BudgetItem.new('kids', -100, 0, INFINITE_DURATION),
+                          BudgetItem.new('income', 200, 0, INFINITE_DURATION)],
+                         [Account.new('investments', 0, 0)])
+        year2 = year1.nextYear
+        expect(year2.accounts[0].amount).to eq(100)
       end
     end
     describe 'extraCash' do
       it 'adds up the items' do
-        year1 = Year.new([BudgetItem.recurring_expense("kids", 100), BudgetItem.recurring_income("work", 100)])
+        year1 = Year.new([BudgetItem.recurring_expense('kids', 100), BudgetItem.recurring_income('work', 100)])
         expect(year1.extraCash).to eq 0
       end
     end
     describe 'merge' do
       it 'merges two years items' do
-        year1 = Year.new([BudgetItem.recurring_expense("kids", 100)])
-        year2 = Year.new([BudgetItem.recurring_expense("kids", 100)])
-        expect(year1.merge(year2).items.length).to eq(2)
+        year1 = Year.new([BudgetItem.recurring_expense('kids', 100)], [Account.new('acct', 100, 0)])
+        year2 = Year.new([BudgetItem.recurring_expense('kids', 100)], [Account.new('acct', 100, 0)])
+        merged = year1.merge(year2)
+        expect(merged.items.length).to eq(2)
+        expect(merged.accounts.length).to eq(2)
       end
     end
   end
@@ -90,50 +110,57 @@ describe 'Finances' do
   describe 'Plans' do
     describe 'generating future years' do
       it 'generates future years from one year of data' do
-        year0 = Year.new([BudgetItem.recurring_expense("kids", 100), BudgetItem.recurring_income("income", 100)])
-        plan = Plan.new({0 => year0})
+        year0 = Year.new([BudgetItem.recurring_expense('kids', 100), BudgetItem.recurring_income('income', 100)])
+        plan = Plan.new({ 0 => year0 })
         ten_year_plan = plan.run_years(10)
         expect(ten_year_plan.years.length).to eq(11)
         expect(ten_year_plan.current_year).to eq(10)
         expect(ten_year_plan.years[10].items[1].amount).to eq(134.3916379344122)
       end
-      it 'generates future years with correct data' do
-        year0 = Year.new([BudgetItem.recurring_expense("kids", 100), BudgetItem.recurring_income("income", 200)])
-        plan = Plan.new({0 => year0})
+      it 'generates future years with correct extra cash' do
+        year0 = Year.new([BudgetItem.recurring_expense('kids', 100), BudgetItem.recurring_income('income', 200)])
+        plan = Plan.new({ 0 => year0 })
         three_year_plan = plan.run_years(3)
-        amounts = three_year_plan.years.map {|k, v| v.extraCash}
-        expect(amounts).to eq([100, 103.0, 106.09, 109.2727])
+        cash_amounts = three_year_plan.years.map { |_k, v| v.extraCash }
+        expect(cash_amounts).to eq([100, 103.0, 106.09, 109.2727])
       end
       it 'treats one time expenses in year 0 properly' do
-        year0 = Year.new([BudgetItem.recurring_expense("kids", 100), BudgetItem.recurring_income("income", 200), BudgetItem.one_time_expense("onetime", 100)])
-        plan = Plan.new({0 => year0})
+        year0 = Year.new([BudgetItem.recurring_expense('kids', 100), BudgetItem.recurring_income('income', 200),
+                          BudgetItem.one_time_expense('onetime', 100)])
+        plan = Plan.new({ 0 => year0 })
         three_year_plan = plan.run_years(3)
-        amounts = three_year_plan.years.map {|k, v| v.extraCash}
+        amounts = three_year_plan.years.map { |_k, v| v.extraCash }
         expect(amounts).to eq([0, 103.0, 106.09, 109.2727])
       end
       it 'treats future year one time expenses properly' do
-        year0 = Year.new([BudgetItem.recurring_expense("kids", 100), BudgetItem.recurring_income("income", 200)])
-        year3 = Year.new([BudgetItem.one_time_expense("kids", 100)])
-        plan = Plan.new({0 => year0, 3 => year3})
+        year0 = Year.new([BudgetItem.recurring_expense('kids', 100), BudgetItem.recurring_income('income', 200)])
+        year3 = Year.new([BudgetItem.one_time_expense('kids', 100)])
+        plan = Plan.new({ 0 => year0, 3 => year3 })
         three_year_plan = plan.run_years(3)
-        amounts = three_year_plan.years.map {|k, v| v.extraCash}
+        amounts = three_year_plan.years.map { |_k, v| v.extraCash }
         expect(amounts).to eq([100, 103.0, 106.09, 9.2727])
       end
     end
     describe 'CSV conversion' do
       it 'prepares for csv conversion properly' do
-        year0 = Year.new([BudgetItem.recurring_expense("kids", 100), BudgetItem.recurring_income("income", 200)])
-        year3 = Year.new([BudgetItem.one_time_expense("kids", 100)])
-        plan = Plan.new({0 => year0, 3 => year3})
+        year0 = Year.new([BudgetItem.recurring_expense('kids', 100), BudgetItem.recurring_income('income', 200)])
+        year3 = Year.new([BudgetItem.one_time_expense('kids', 100)])
+        plan = Plan.new({ 0 => year0, 3 => year3 })
         csvs = plan.toCSV
-        expect(csvs).to eq({0 =>{ 
-                                "items"=>[{"name"=>"kids", "amount"=>-100, "rate_of_increase"=>0.03, "duration"=>:infinite},
-                                          {"name"=>"income", "amount"=>200, "rate_of_increase" => 0.03, "duration"=>:infinite}],
-                                },
-                            3 => {"items"=>[{"name"=>"kids", "amount"=>-100, "rate_of_increase"=>0, "duration"=>1}]}
-        })
+        expect(csvs).to eq({ 0 => {
+                               'items' => [{ 'name' => 'kids', 'amount' => -100, 'rate_of_increase' => 0.03, 'duration' => :infinite },
+                                           { 'name' => 'income', 'amount' => 200, 'rate_of_increase' => 0.03,
+                                             'duration' => :infinite }]
+                             },
+                             3 => { 'items' => [{ 'name' => 'kids', 'amount' => -100, 'rate_of_increase' => 0,
+                                                  'duration' => 1 }] } })
       end
     end
   end
+  describe 'my plan' do
+    it 'looks good?' do
+      result = MY_PLAN.run_years(30)
+      puts result.years[30].accounts.map { |a| [a.name, a.amount] }
+    end
+  end
 end
-
